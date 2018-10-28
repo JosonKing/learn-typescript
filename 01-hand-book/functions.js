f917f72a806a5b68ff5aa4c43d2faed5f8747b15
josonking <wangsanmu5@163.com>
2018-10-28 22:44:58 +0800

    update 11-19

--- 01-hand-book/functions.js
+++ 01-hand-book/functions.js
@@ -20,9 +20,54 @@ exports.__esModule = true;
 /**
  * 可选参数和默认参数
  */
-function buildName(firstName, lastName) {
-    return firstName + " " + lastName;
+// function buildName(firstName: string, lastName: string) {
+//   return firstName + " " + lastName;
+// }
+// let result1 = buildName("Bob");                  // error, too few parameters
+// let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+// let result3 = buildName("Bob", "Adams");         // ah, just right
+// function buildName(firstName: string, lastName?: string) {
+//   if (lastName)
+//     return firstName + " " + lastName;
+//   else
+//     return firstName;
+// }
+// let result1 = buildName("Bob");  // works correctly now
+// let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+// let result3 = buildName("Bob", "Adams");  // ah, just right
+// function buildName(firstName: string, lastName = "Smith") {
+//   return firstName + " " + lastName;
+// }
+// let result1 = buildName("Bob");                  // works correctly now, returns "Bob Smith"
+// let result2 = buildName("Bob", undefined);       // still works, also returns "Bob Smith"
+// let result3 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+// let result4 = buildName("Bob", "Adams");         // ah, just right
+/**
+ * 剩余参数
+ */
+// function buildName(firstName: string, ...restOfName: string[]) {
+//   return firstName + " " + restOfName.join(" ");
+// }
+// let employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+/**
+ * 重载
+ */
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        var pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
 }
-var result1 = buildName("Bob"); // error, too few parameters
-var result2 = buildName("Bob", "Adams", "Sr."); // error, too many parameters
-var result3 = buildName("Bob", "Adams"); // ah, just right
+var myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+var pickedCard1 = myDeck[pickCard(myDeck)];
+alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+var pickedCard2 = pickCard(15);
+alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
