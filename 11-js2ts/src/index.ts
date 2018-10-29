ab0fb1515748a0981b5f5106d9b8493fbd31862b
josonking <wangsanmu5@163.com>
2018-10-29 14:16:55 +0800

    update 11 & 12 files

--- 11-js2ts/src/index.ts
+++ 11-js2ts/src/index.ts
@@ -1,47 +1,63 @@
+'use strict';
+
 // import 'babel-polyfill';
 // require('babel-polyfill');
 // var FileSaver = require('file-saver');
-import utils = require('../src/utils');
+import Utils = require("./Utils");
 
-let logger;
+class Logger {
 
-logger.config = function(level, color, appender) {
-  this.level = level || 'ERROR';
-  this.color = color || 'black';
-  this.appender = appender || 'console';
-};
+  private name: string;
+  private level: string;
+  private color: string;
+  private appender: string;
 
-logger.log = function(...message) {
-  if (utils.shouldLog(this.level, 0)) {
-    if (this.appender.toLowerCase() == 'file') {
-      // var blob = new Blob(["Hello, world!"], {
-      //   type: "text/plain;charset=utf-8"
-      // });
-      // FileSaver.saveAs(blob, "helloworld.txt");
-    } else {
-      console.log(utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'LOG', '=>', ...message);
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public config (level, color, appender) {
+    this.level = level || 'ERROR';
+    this.color = color || 'black';
+    this.appender = appender || 'console';
+  }
+
+  public log (...message) {
+    if (Utils.shouldLog(this.level, 0)) {
+      if (this.appender.toLowerCase() == 'file') {
+        // var blob = new Blob(["Hello, world!"], {
+        //   type: "text/plain;charset=utf-8"
+        // });
+        // FileSaver.saveAs(blob, "helloworld.txt");
+      } else {
+        console.log(Utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'LOG', this.name, '=>', ...message);
+      }
     }
   }
-};
-logger.info = function(...message) {
-  if (utils.shouldLog(this.level, 1)) {
-    console.info(utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'INFO', '=>', ...message);
-  }
-};
-logger.debug = function(...message) {
-  if (utils.shouldLog(this.level, 2)) {
-    console.debug(utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'DEBUG', '=>', ...message);
-  }
-};
-logger.warn = function(...message) {
-  if (utils.shouldLog(this.level, 3)) {
-    console.warn(utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'WARN', '=>', ...message);
-  }
-};
-logger.error = function(...message) {
-  if (utils.shouldLog(this.level, 4)) {
-    console.error(utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'ERROR', '=>', ...message);
-  }
-};
 
-export = logger;
+  public info (...message) {
+    if (Utils.shouldLog(this.level, 1)) {
+      console.info(Utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'INFO', this.name, '=>', ...message);
+    }
+  }
+
+  public debug (...message) {
+    if (Utils.shouldLog(this.level, 2)) {
+      console.debug(Utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'DEBUG', this.name, '=>', ...message);
+    }
+  }
+
+  public warn (...message) {
+    if (Utils.shouldLog(this.level, 3)) {
+      console.warn(Utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'WARN', this.name, '=>', ...message);
+    }
+  }
+
+  public error(...message) {
+    if (Utils.shouldLog(this.level, 4)) {
+      console.error(Utils.dateFtt('yyyy-MM-dd hh:mm:ss.S', new Date()), 'ERROR', this.name, '=>', ...message);
+    }
+  }
+}
+
+export = Logger;
